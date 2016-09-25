680029437f50d2a5e0898426b39717939761bc31
Stefan Pulido <estefanpulido52@gmail.com>
2016-09-24 23:22:57 -0500

    if-else

---
+++ p2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main (void){
+  
+  int x{};
+  
+  std::cin >> x;
+  
+  if (x==100)
+    std::cout << "x is 100" << std::endl;
+  
+  else
+    std:: cout << "x is not 100" << std:: endl;
+  
+  return 0;
+}
