f2549823fe00f3109028efaf056227c79f998887
remesap <remesap@unal.edu.co>
2017-02-28 16:25:51 -0500

    Create loop_for1.cpp

---
+++ loop_for1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+int main(void){
+   
+  for (int n=10; n>0 ; --n){
+ 
+    std::cout << n << ", ";
+ 
+  };
+ 
+ 
+ 
+  std::cout << "liftoff!" << std::endl;
+  return 0;
+}
+ 
