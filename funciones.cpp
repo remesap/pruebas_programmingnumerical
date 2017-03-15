7bf70a53705e9db085c557d27b96fb16cf28b754
remesap <remesap@unal.edu.co>
2017-03-15 07:44:56 -0500

    pr...

---
+++ funciones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+double seno (double x);
+
+int main (void){
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+
+  double x = 0.1;
+  std::cout << seno(x) << std::endl;
+  std::cout << "&x en main: " << &x << std::endl;
+  return 0;
+}
+
+double seno (double x)
+{
+  std::cout << "&x en seno: " << &x << std::endl;
+  double val = x -  x*x*x/6.0;
+  return val;
+}
